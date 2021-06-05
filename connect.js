609babdd0f964bb0049f6cc9efa85e63550bad37
chintan <chintantripathi1310@gmail.com>
2021-06-05 13:17:23 +0530

    practice done.

---
+++ connect.js
@@ -0,0 +1,17 @@
+function myFun(){
+
+    var frst = document.getElementById("link");
+    var blck = document.getElementById("extrn");
+    var frm = document.getElementById("form");
+
+    blck.style.display = "block";
+    frm.style.display = "none";
+}
+
+function myFun1(){
+
+    var frst = document.getElementById("link");
+    var blck = document.getElementById("extrn");
+
+    blck.style.display="none";
+}
